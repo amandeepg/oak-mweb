83f1f8ce50f43073fd55fe709f73f3ba6310bbc1
Amandeep Grewal <me@amandeep.ca>
2013-08-31 23:01:05 -0400

    Add minimal support for adding courses.
    Allow additions of courses using javascript window prompts.
    Also blocks routes change for listing all courses until data is loaded.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('OakMwebApp')
-  .controller('MainCtrl', function ($scope, Course) {
-    $scope.courses = Course.query();
+  .controller('MainCtrl', function ($scope, Course, courses) {
+    $scope.courses = courses;
 
-    $scope.addCourse = function(){
-      window.alert('blah');
+    $scope.addCourse = function() {
+      var courseCode = window.prompt('Course code?');
+      var coursePassword = window.prompt('Course password?');
+      Course.save({
+        // Using hack of password in POST until server supports real auth.
+        // TODO(amandeepg): fix above hack
+        password: 'EngSci',
+        courseCode: courseCode,
+        coursePassword: coursePassword
+      }, function() {
+        $scope.courses = Course.query();
+      });
     };
   });
